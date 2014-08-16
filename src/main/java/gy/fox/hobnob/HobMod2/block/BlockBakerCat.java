b0265fd877905cb972a6618029d238de2e50545a
hobnob11 <williamhobson62@aol.com>
2014-08-16 13:11:20 +0100

    Bakercat more progress
    Now sort of works!

--- src/main/java/gy/fox/hobnob/HobMod2/block/BlockBakerCat.java
+++ src/main/java/gy/fox/hobnob/HobMod2/block/BlockBakerCat.java
@@ -1,11 +1,13 @@
 package gy.fox.hobnob.HobMod2.block;
 
+import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import gy.fox.hobnob.HobMod2.utility.LogHelper;
 import net.minecraft.block.Block;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.util.IIcon;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
@@ -18,17 +20,36 @@ public class BlockBakerCat extends BlockHM2 {
         this.setBlockName("bakercat");
         this.setStepSound(Block.soundTypeMetal);
     }
+    protected IIcon blockIcon;
+    protected IIcon blockIconFront;
+
     @Override
     @SideOnly(Side.CLIENT)
     public void registerBlockIcons(IIconRegister iconRegister)
     {
-        blockIcon = iconRegister.registerIcon("hobmod2:catBakerActiveBlock");
+        blockIcon = iconRegister.registerIcon("hobmod2:catBakerBlock");
+        blockIconFront = iconRegister.registerIcon("hobmod2:catBakerActiveBlock");
     }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public IIcon getIcon(int side, int meta)
+    {
+        if(side==2&&meta==1)
+        {
+            return blockIconFront;
+        }
+        else
+        {
+            return blockIcon;
+        }
+    }
+
     private boolean powered;
     @Override
     public void onNeighborBlockChange(World para1_world, int para2, int para3, int para4, Block para5)
     {
-        if(para1_world.isRemote) {
+        if(FMLCommonHandler.instance().getEffectiveSide() == Side.SERVER) {
             if (para1_world.isBlockIndirectlyGettingPowered(para2, para3, para4) && !powered) {
                 powered=true;
                 para1_world.setBlockMetadataWithNotify(para2, para3, para4, 1, 2);
@@ -37,7 +58,7 @@ public class BlockBakerCat extends BlockHM2 {
             else if(!para1_world.isBlockIndirectlyGettingPowered(para2,para3,para4)&& powered) {
                 powered=false;
                 para1_world.setBlockMetadataWithNotify(para2, para3, para4, 0, 2);
-                LogHelper.info("metadata 0");
+                LogHelper.info("Metadata 0");
             }
         }
     }
