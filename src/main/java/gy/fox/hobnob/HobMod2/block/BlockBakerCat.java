144a2e6300fb6642a96b91e65e49822ef22a9298
hobnob11 <williamhobson62@aol.com>
2014-08-16 08:35:01 +0100

    Working on BakerCat block
    its comming back! veeeeery slowly...

---
+++ src/main/java/gy/fox/hobnob/HobMod2/block/BlockBakerCat.java
@@ -0,0 +1,15 @@
+package gy.fox.hobnob.HobMod2.block;
+
+import net.minecraft.block.Block;
+
+public class BlockBakerCat extends BlockHM2 {
+
+    public BlockBakerCat()
+    {
+        super();
+        this.setBlockName("bakercat");
+        this.setStepSound(Block.soundTypeMetal);
+        this.setBlockTextureName("HobMod2:flag.png");
+    }
+
+}
--- src/main/java/gy/fox/hobnob/HobMod2/init/ModBlocks.java
+++ src/main/java/gy/fox/hobnob/HobMod2/init/ModBlocks.java
@@ -1,14 +1,17 @@
 package gy.fox.hobnob.HobMod2.init;
 
 import cpw.mods.fml.common.registry.GameRegistry;
+import gy.fox.hobnob.HobMod2.block.BlockBakerCat;
 import gy.fox.hobnob.HobMod2.block.BlockFlag;
 import gy.fox.hobnob.HobMod2.block.BlockHM2;
 
 public class ModBlocks {
     public static final BlockHM2 flag = new BlockFlag();
+    public static final BlockHM2 bakercat = new BlockBakerCat();
 
     public static void init(){
 
         GameRegistry.registerBlock(flag,"flag");
+        GameRegistry.registerBlock(bakercat,"bakercat");
     }
 }
