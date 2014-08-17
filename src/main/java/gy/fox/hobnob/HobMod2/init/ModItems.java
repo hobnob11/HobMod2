bc5023a9f7b78eb9371b58a677c54ddd20b7f7df
hobnob11 <williamhobson62@aol.com>
2014-08-17 08:52:21 +0100

    Fancy texture is Fancy
    assets/textures/items/records.rbc.png
    :D

--- src/main/java/gy/fox/hobnob/HobMod2/init/ModItems.java
+++ src/main/java/gy/fox/hobnob/HobMod2/init/ModItems.java
@@ -4,6 +4,7 @@ import cpw.mods.fml.common.registry.GameRegistry;
 import gy.fox.hobnob.HobMod2.item.ItemCustomRecord;
 import gy.fox.hobnob.HobMod2.item.ItemHM2;
 import gy.fox.hobnob.HobMod2.item.ItemMapleLeaf;
+import gy.fox.hobnob.HobMod2.utility.LogHelper;
 import net.minecraft.item.ItemRecord;
 
 public class ModItems {
@@ -11,11 +12,11 @@ public class ModItems {
     public static final ItemHM2 mapleLeaf = new ItemMapleLeaf();
     public static ItemCustomRecord rbc;
 
-    @SuppressWarnings("RedundantCast")
     public static void init()
     {
         GameRegistry.registerItem(mapleLeaf, "mapleLeaf");
         GameRegistry.registerItem(rbc = new ItemCustomRecord("rbc", "Baker Cat :D", new String[]{"Improves productivity 20%!", "Only if you are pusheen"}).setDomain("hobmod2"), "rbc");
+        LogHelper.info(rbc.getRecordNameLocal());
     }
 
 }
