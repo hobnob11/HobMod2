2bf0f9c907c72faad264214a6859f954cf0aaf5a
ShadowBlade50 <werschy.m@gmail.com>
2014-08-20 00:04:34 +0200

    Fix container and add saving to the CookieBank.

--- src/main/java/gy/fox/hobnob/HobMod2/inventory/CookieBank.java
+++ src/main/java/gy/fox/hobnob/HobMod2/inventory/CookieBank.java
@@ -3,11 +3,12 @@ package gy.fox.hobnob.HobMod2.inventory;
 import gy.fox.hobnob.HobMod2.te.CookieBankTE;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
-import net.minecraft.inventory.*;
+import net.minecraft.inventory.Container;
+import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
 
 
-public class CookieBank extends net.minecraft.inventory.Container
+public class CookieBank extends Container
 {
     private CookieBankTE tile;
 
@@ -17,50 +18,50 @@ public class CookieBank extends net.minecraft.inventory.Container
         bindPlayerInventory(inventory);
     }
 
-
     /*
     Add slots to our GUI.
     The id's are for the slotnumbers.
     For the rest, the i * 18 and j * 18 is always the same.
     The other numbers can change, depending on your gui.
      */
-
     private void bindPlayerInventory(InventoryPlayer inventoryPlayer)
     {
-        int id = 0; //player inv id
-        int id2 =0;
+        // Hotbar (Slot 0 - 8)
+        for(int i = 0; i < 9; i++)
+        {
+            addSlotToContainer(new Slot(inventoryPlayer, i, i * 18 + 8, 189));
+        }
 
-        for(int i =0; i<9; i++)
+        // Player Inventory (Slot 9 - 35)
+        for(int i = 0; i < 3; i++)
         {
-            addSlotToContainer(new Slot(inventoryPlayer, id, i*18 +8,189)); //adds the hotbar apperntly
-            id++;
-        }
-        for(int i =0; i < 3; i++)
-        {
-            for(int j=0; j<9;j++)
+            for(int j = 0; j < 9; j++)
             {
-                addSlotToContainer(new Slot(inventoryPlayer,id,j*18+8,i*18+131));//adds the whole inventory soemhow
+                addSlotToContainer(new Slot(inventoryPlayer, i * 9 + j + 9, j * 18 + 8, i * 18 + 131));
             }
         }
-        for(int i = 0; i < 3; i ++)
+
+        // Block Inventory
+        for(int i = 0; i < 2; i++)
         {
-            for(int j = 0; j < 2; j++)
+            for(int j = 0; j < 3; j++)
             {
-                addSlotToContainer(new SlotTest(tile, id2, i * 18 + 62, j * 18 + 21)); //Adds custon slots
-                id2++;
+                addSlotToContainer(new SlotTest(tile, i * 3 + j, j * 18 + 62, i * 18 + 21));
             }
         }
-        addSlotToContainer(new SlotTest(tile, id2, 81, 95));
+
+        addSlotToContainer(new SlotTest(tile, 6, 81, 95));
     }
+
     @Override
     public ItemStack transferStackInSlot(EntityPlayer par1EntityPlayer, int slotIndex)
     {
         return null;
     }
+
     @Override
     public boolean canInteractWith(EntityPlayer var1)
     {
         return true;
     }
-
 }
